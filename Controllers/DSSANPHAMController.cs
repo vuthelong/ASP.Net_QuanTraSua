807216d7c08393a0e4a0966240b50edcc7945edb
Sang Triệu <trieutrungtansang@gmail.com>
2022-02-18 16:29:26 +0700

    them controller DSSANPHAM

---
+++ Controllers/DSSANPHAMController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_QuanTraSua.Controllers
+{
+    public class DSSANPHAMController : Controller
+    {
+        // GET: DSSANPHAM
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
